a6fe1efb14b6c37233fa86cc878d72b233c95c8a
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2022-11-15 02:07:54 +0100

    working skill animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { IntroductionComponent } from './introduction/introduction.component';
+import { JourneyComponent } from './journey/journey.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 const routes: Routes = [
 { path: '', component: IntroductionComponent },
-{ path: 'legal-notice', component: LegalNoticeComponent }
+{ path: 'legal-notice', component: LegalNoticeComponent },
+{ path: 'about', component: JourneyComponent}
+
 ];
 
 @NgModule({
