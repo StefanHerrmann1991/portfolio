29f2b307a7588bef1754ead826ae880f302b6d9f
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-06-07 10:17:33 +0200

    neues design

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -27,6 +27,7 @@ export class ProjectsComponent implements OnInit {
       'name': 'El Pollo Loco',
       'description': 'A JavaScript-based, object-oriented jump-and-run game, similar to Super Mario.',
       'path': 'https://stefan-herrmann.developerakademie.net/ElPolloLoco',
+      'github':'https://github.com/StefanHerrmann1991/El-Pollo-Loco',
       'img': 'assets/img/projects/El Polo Loco.png',
       'language': 'JavaScript'
     },
@@ -34,12 +35,14 @@ export class ProjectsComponent implements OnInit {
       'name': 'Join',
       'description': 'A JavaScript-built Kanban board for team project creation.',
       'path': 'http://stefan-herrmann.developerakademie.net/Join/main/00login-register/login.html',
+      'github':'https://github.com/StefanHerrmann1991/Join',
       'img': 'assets/img/projects/join.png',
       'language': 'JavaScript'
     }, {
       'name': 'Portfolio',
       'description': 'An Angular-developed portfolio showcasing personal work.',
-      'path': '',
+      'path': 'https://stefan-herrmann.developerakademie.net/stefan-herrmann/',
+      'github':'https://github.com/StefanHerrmann1991/portfolio',
       'img': 'assets/img/projects/portfolio.png',
       'language': 'Angular'
     }
@@ -47,6 +50,7 @@ export class ProjectsComponent implements OnInit {
       'name': 'Pokedex',
       'description': 'A JavaScript-implemented Pokedex leveraging REST API.',
       'path': 'https://stefan-herrmann.developerakademie.net/Pokedex',
+      'github':'https://github.com/StefanHerrmann1991/Pokedex',
       'img': 'assets/img/projects/pokedex.jpg',
       'language': 'JavaScript'
     }
