0f50ca4ccc753cc8f7d7d1bde3330f9d78ce310b
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-06-11 02:40:28 +0200

    working animations in project component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -10,10 +10,11 @@ import AOS from 'aos';
 export class ProjectsComponent implements OnInit {
   console = console;
   actualProject: string;
-
+  isHovering: { [key: number]: boolean } = {};
   showTitle: number = -1;
   constructor() {
 
+  
   }
 
 
@@ -27,7 +28,7 @@ export class ProjectsComponent implements OnInit {
       'name': 'El Pollo Loco',
       'description': 'A JavaScript-based, object-oriented jump-and-run game. Help Pepe to rescue his wife from the bad chickens.',
       'path': 'https://stefan-herrmann.developerakademie.net/ElPolloLoco',
-      'github':'https://github.com/StefanHerrmann1991/El-Pollo-Loco',
+      'github': 'https://github.com/StefanHerrmann1991/El-Pollo-Loco',
       'img': 'assets/img/projects/El Polo Loco.png',
       'technologies': 'JavaScript | HTML | CSS'
     },
@@ -35,15 +36,15 @@ export class ProjectsComponent implements OnInit {
       'name': 'Join',
       'description': 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
       'path': 'http://stefan-herrmann.developerakademie.net/Join/main/00login-register/login.html',
-      'github':'https://github.com/StefanHerrmann1991/Join',
-      'img': 'assets/img/projects/join.png',     
+      'github': 'https://github.com/StefanHerrmann1991/Join',
+      'img': 'assets/img/projects/join.png',
       'technologies': 'JavaScript | HTML | SASS'
     }, {
       'name': 'Portfolio',
       'description': 'An Angular-developed portfolio showcasing personal work.',
       'path': 'https://stefan-herrmann.developerakademie.net/stefan-herrmann/',
-      'github':'https://github.com/StefanHerrmann1991/portfolio',
-      'img': 'assets/img/projects/portfolio.png',      
+      'github': 'https://github.com/StefanHerrmann1991/portfolio',
+      'img': 'assets/img/projects/portfolio.png',
       'technologies': 'Angular | TypeScript | SASS'
     }
     ,/*  {
