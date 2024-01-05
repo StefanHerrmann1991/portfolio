7785c8d7b1f1850ac340c543cbb0c2778027e8d9
StefanHerrmann1991 <sth1812@posteo.de>
2024-01-06 00:50:14 +0100

    better imprint style

--- src/app/journey/journey.component.ts
+++ src/app/journey/journey.component.ts
@@ -9,14 +9,14 @@ import { DisplayService } from '../display.service';
 })
 export class JourneyComponent implements OnInit {
 
-  constructor(public displayService: DisplayService) {}
+  constructor(public displayService: DisplayService) { }
 
   ngOnInit(): void {
     AOS.init();
   }
 
   journeys = [{
-    'text': "I'm Stefan Herrmann, a Front-end Developer driven by a love for coding based in Berlin Germany. During my master's in Life Science Engineering, I discovered the power of algorithms and analytical thinking in solving various challenges."
+    'text': "I'm Stefan Herrmann, a Front-end Developer with a Master in Life Science Engineering. I developed an analytical and structured mindset, realizing the potential of algorithms in solving problems. Focused on explosives, cell culture, and immunological testing, I thrived in data analysis and statistical discussions on the computer. I joined the Developer Akademie for professional guidance and to create high-quality code, combining the best of research with greater speed."
   }
 
   ]
